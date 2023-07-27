69582987fc7abde962d1378f6e21c1e95a425112
Ndumiso Thela <ndumisothela480@gmail.com>
2023-07-27 03:39:27 +0200

    age function

--- welcome.js
+++ welcome.js
@@ -3,3 +3,12 @@ const greeting = "Hello World"
  const welcome = document.getElementById("header")
  welcome.textContent= `${greeting}, my name is ${userName}`
 console.log(`${greeting} , my name is ${userName}`)
+
+function age(){
+    let yearBorn = Number(prompt("which year were you born ?"))
+    let currentAge = 2023 - yearBorn;
+    const howOld = document.getElementById("userAge")
+    howOld.textContent = `I am ${currentAge} years old`
+    console.log(`I am ${currentAge} years old`)
+ }
+ age()
