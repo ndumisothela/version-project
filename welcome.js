5986ede78487fe7bda58b2915467bd69942512c3
Ndumiso Thela <ndumisothela480@gmail.com>
2023-07-27 03:27:16 +0200

    first commit

---
+++ welcome.js
@@ -0,0 +1,5 @@
+const greeting = "Hello World"
+ const userName = prompt("Please enter your name");
+ const welcome = document.getElementById("header")
+ welcome.textContent= `${greeting}, my name is ${userName}`
+console.log(`${greeting} , my name is ${userName}`)
